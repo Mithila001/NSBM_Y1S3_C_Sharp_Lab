ff60bcb6e8c875a423c92ef52a89e97d56b0de28
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-06 11:21:27 +0530

    New Project

---
+++ Lab_2024_02_31_LabSheet_1_Q7/Lab_2024_02_31_LabSheet_1_Q7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2024_02_31_LabSheet_1_Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
