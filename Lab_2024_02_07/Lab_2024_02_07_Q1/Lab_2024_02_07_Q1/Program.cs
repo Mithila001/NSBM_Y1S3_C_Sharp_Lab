7a18cd4ef393776d3b5986e8819228149d32d131
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-07 13:27:15 +0530

    Q1

--- Lab_2024_02_07/Lab_2024_02_07_Q1/Lab_2024_02_07_Q1/Program.cs
+++ Lab_2024_02_07/Lab_2024_02_07_Q1/Lab_2024_02_07_Q1/Program.cs
@@ -10,6 +10,21 @@ namespace Lab_2024_02_07_Q1
     {
         static void Main(string[] args)
         {
+            Book book1 = new Book();
+            book1.properties = "Sci fi";
+            book1.author = "Bard";
+            book1.title = "How to Sleep";
+
+            Console.WriteLine("Title: "+ book1.title);
+            Console.WriteLine("Proterties"+ book1.properties);
+            Console.WriteLine("Author: "+book1.author);
         }
+    }
+
+    public class Book
+    {
+        public string properties;
+        public string title;
+        public string author;
     }
 }
