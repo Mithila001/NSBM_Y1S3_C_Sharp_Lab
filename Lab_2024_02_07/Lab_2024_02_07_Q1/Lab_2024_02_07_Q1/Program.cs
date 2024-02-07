da9a76bbd88012d693ddb6b80ef9d925a55e2803
Mithila001 <mithiladissanayakaofficial@gmail.com>
2024-02-07 13:29:06 +0530

    Q2 ss

--- Lab_2024_02_07/Lab_2024_02_07_Q1/Lab_2024_02_07_Q1/Program.cs
+++ Lab_2024_02_07/Lab_2024_02_07_Q1/Lab_2024_02_07_Q1/Program.cs
@@ -4,20 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_2024_02_07_Q1
+namespace Lab_2024_02_07_Q2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.properties = "Sci fi";
-            book1.author = "Bard";
-            book1.title = "How to Sleep";
-
-            Console.WriteLine("Title: "+ book1.title);
-            Console.WriteLine("Proterties"+ book1.properties);
-            Console.WriteLine("Author: "+book1.author);
+    
         }
     }
 
